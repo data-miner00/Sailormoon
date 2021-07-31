cba4d64b1c1d17d698cf09c867ca7c9cdf842af8
data-miner00 <data-miner00@users.noreply.github.com>
2021-07-31 22:56:35 +0800

    feat: Added typing effect

--- src/events/message.ts
+++ src/events/message.ts
@@ -24,35 +24,55 @@ export default (bot: Client): void => {
 
     //
     if (greeting.greeting.includes(messageLower)) {
+      message.channel.startTyping();
       let randomIndex: number = Math.floor(
         Math.random() * greeting.greeting.length
       );
       console.log(randomIndex);
       let mentionUser: string =
         Math.random() >= 0.5 ? `<@!${message.author.id}>` : "";
-      message.channel.send(`${mentionUser} ${greeting.greeting[randomIndex]}`);
+
+      setTimeout(() => {
+        message.channel
+          .send(`${mentionUser} ${greeting.greeting[randomIndex]}`)
+          .then(function (_: Message): void {
+            message.channel.stopTyping();
+          });
+      }, 2000);
     }
 
     if (messageLower.includes("joke")) {
+      message.channel.startTyping();
       let randomIndex: number = Math.floor(
         Math.random() * greeting.emoji.length
       );
       axios.get(jokeAPI).then((response) => {
-        message.channel.send(
-          `<@!${message.author.id}> ${greeting.emoji[randomIndex]}  \`\`\`${response["data"].joke}\`\`\``
-        );
+        message.channel
+          .send(
+            `<@!${message.author.id}> ${greeting.emoji[randomIndex]}  \`\`\`${response["data"].joke}\`\`\``
+          )
+          .then(function (_: Message): void {
+            message.channel.stopTyping();
+          });
       });
     }
 
     if (messageLower.includes("covid")) {
+      message.channel.startTyping();
       axios
         .get(
           "https://api.apify.com/v2/key-value-stores/6t65lJVfs3d8s6aKc/records/LATEST?disableRedirect=true"
         )
         .then((response) => {
-          message.channel.send(`
+          message.channel
+            .send(
+              `
         \`\`\`🤧 Total Malaysia Cases 🤧 \n\nTotal Tested Positive: ${response["data"].testedPositive},\nTotal Recovered: ${response["data"].recovered},\nTotal Active Cases : ${response["data"].activeCases},\nICU : ${response["data"].inICU},\nDeceased: ${response["data"].deceased},\nUpdated: ${response["data"].lastUpdatedAtSource}\n\nPlease take care and stay safe 😁.\`\`\`
-        `);
+        `
+            )
+            .then((_: Message): void => {
+              message.channel.stopTyping();
+            });
         });
     }
 
