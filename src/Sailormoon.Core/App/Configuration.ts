9f4fcee56ff8caa94761bd72f8a5a3d078a448ee
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-15 00:41:05 +0800

    feat: Added msia api url

--- src/Sailormoon.Core/App/Configuration.ts
+++ src/Sailormoon.Core/App/Configuration.ts
@@ -7,6 +7,7 @@ export default class Configuration {
     #dadjokeHost: string;
     #jokeApiHost: string;
     #xrapidApiKey: string;
+    #msiaCovidApiUrl: string;
 
     constructor() {
         if (!process.env.DISCORD_API_TOKEN) {
@@ -19,6 +20,8 @@ export default class Configuration {
         this.#boredApiUrl = "https://www.boredapi.com/api/activity/";
         this.#dadjokeHost = "dad-jokes.p.rapidapi.com";
         this.#jokeApiHost = "jokeapi-v2.p.rapidapi.com";
+        this.#msiaCovidApiUrl =
+            "https://api.apify.com/v2/key-value-stores/6t65lJVfs3d8s6aKc/records/LATEST?disableRedirect=true";
     }
 
     public get commandPrefix(): string {
@@ -43,5 +46,9 @@ export default class Configuration {
 
     public get xRapidApiKey(): string {
         return this.#xrapidApiKey;
+    }
+
+    public get msiaCovidApiUrl(): string {
+        return this.#msiaCovidApiUrl;
     }
 }
