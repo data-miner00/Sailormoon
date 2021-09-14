8851c9b25f4760a2af3388384b19015a60b26e08
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-14 23:00:23 +0800

    feat: Added general utilities

---
+++ src/Sailormoon.Core/Utility/GeneralUtils.ts
@@ -0,0 +1,22 @@
+import { Message, User } from "discord.js";
+import Configuration from "../App/Configuration";
+import CommandObject from "../Command/CommandObject";
+
+export default class GeneralUtils {
+    private static configuration: Configuration = new Configuration();
+
+    public static isBot(user: User): boolean {
+        return user.bot;
+    }
+
+    public static isCommand(message: Message): boolean {
+        return message.content.startsWith(this.configuration.commandPrefix);
+    }
+
+    public static preprocessCommand(message: Message): CommandObject {
+        const args: string[] = message.content.split(" ");
+        const command: string = args.splice(0)[0].slice(1);
+
+        return new CommandObject(command, args);
+    }
+}
