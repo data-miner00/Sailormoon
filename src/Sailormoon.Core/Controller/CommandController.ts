6a77fe9bf88cbd1e54604b2cb82543897cebb940
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-14 23:34:32 +0800

    feat: Updated Command Controller

--- src/Sailormoon.Core/Controller/CommandController.ts
+++ src/Sailormoon.Core/Controller/CommandController.ts
@@ -1,17 +1,50 @@
-import { DMChannel, Message, NewsChannel, TextChannel, User } from "discord.js";
+import { Message, User } from "discord.js";
+import BoredCommand from "../Command/BoredCommand";
+import Command from "../Command/Command";
 import CommandObject from "../Command/CommandObject";
+import PollCommand from "../Command/PollCommand";
+import AllChannel from "../Utility/AllChannel";
 import GeneralUtils from "../Utility/GeneralUtils";
 
 export default class CommandController {
     #commandObj: CommandObject;
     #message: Message;
     #author: User;
-    #channel: TextChannel | DMChannel | NewsChannel;
+    #channel: AllChannel;
+    #command: Command;
 
     constructor(message: Message) {
         this.#message = message;
         this.#commandObj = GeneralUtils.preprocessCommand(message);
         this.#author = message.author;
         this.#channel = message.channel;
+        this.#command = this.searchCommand(this.#commandObj.command);
+
+        this.invokeCommand();
+    }
+
+    private invokeCommand(): void {
+        this.#command.execute();
+    }
+
+    private searchCommand(commandSignature: string): Command {
+        let command: Command;
+        switch (commandSignature) {
+            case "poll":
+                command = new PollCommand(
+                    this.#commandObj,
+                    this.#channel,
+                    this.#author
+                );
+            case "bored":
+                command = new BoredCommand(
+                    this.#commandObj,
+                    this.#channel,
+                    this.#author
+                );
+            default:
+                console.log("No such command");
+        }
+        return command;
     }
 }
