eb37fad235a1de6d50edf6c7e9ab90b1e012f316
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-15 00:13:31 +0800

    refactor: Migrate host to config

--- src/Sailormoon.Core/App/Configuration.ts
+++ src/Sailormoon.Core/App/Configuration.ts
@@ -3,6 +3,10 @@ import EnvLoader from "../Utility/EnvLoader";
 export default class Configuration {
     #commandPrefix: string;
     #discordToken: string;
+    #boredApiUrl: string;
+    #dadjokeHost: string;
+    #jokeApiHost: string;
+    #xrapidApiKey: string;
 
     constructor() {
         if (!process.env.DISCORD_API_TOKEN) {
@@ -11,6 +15,10 @@ export default class Configuration {
 
         this.#commandPrefix = "?";
         this.#discordToken = process.env.DISCORD_API_TOKEN;
+        this.#xrapidApiKey = process.env.X_RAPIDAPI_KEY;
+        this.#boredApiUrl = "https://www.boredapi.com/api/activity/";
+        this.#dadjokeHost = "dad-jokes.p.rapidapi.com";
+        this.#jokeApiHost = "jokeapi-v2.p.rapidapi.com";
     }
 
     public get commandPrefix(): string {
@@ -19,5 +27,21 @@ export default class Configuration {
 
     public get discordToken(): string {
         return this.#discordToken;
+    }
+
+    public get boredApiUrl(): string {
+        return this.#boredApiUrl;
+    }
+
+    public get dadjokeHost(): string {
+        return this.#dadjokeHost;
+    }
+
+    public get jokeApiHost(): string {
+        return this.#jokeApiHost;
+    }
+
+    public get xRapidApiKey(): string {
+        return this.#xrapidApiKey;
     }
 }
