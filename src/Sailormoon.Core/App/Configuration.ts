23487f22c2745d3195087677fde2850127b0c1ec
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-16 19:41:49 +0800

    refactor: Relocate some url

--- src/Sailormoon.Core/App/Configuration.ts
+++ src/Sailormoon.Core/App/Configuration.ts
@@ -3,11 +3,9 @@ import EnvHelper from "../Utility/EnvHelper";
 export default class Configuration {
     #commandPrefix: string;
     #discordToken: string;
-    #boredApiUrl: string;
     #dadjokeHost: string;
     #jokeApiHost: string;
     #xrapidApiKey: string;
-    #msiaCovidApiUrl: string;
 
     private static instance: Configuration;
 
@@ -17,11 +15,8 @@ export default class Configuration {
         this.#commandPrefix = "?";
         this.#discordToken = EnvHelper.queryEnv("discord");
         this.#xrapidApiKey = EnvHelper.queryEnv("rapidapi");
-        this.#boredApiUrl = "https://www.boredapi.com/api/activity/";
         this.#dadjokeHost = "dad-jokes.p.rapidapi.com";
         this.#jokeApiHost = "jokeapi-v2.p.rapidapi.com";
-        this.#msiaCovidApiUrl =
-            "https://api.apify.com/v2/key-value-stores/6t65lJVfs3d8s6aKc/records/LATEST?disableRedirect=true";
     }
 
     public static getInstance(): Configuration {
@@ -39,10 +34,6 @@ export default class Configuration {
         return this.#discordToken;
     }
 
-    public get boredApiUrl(): string {
-        return this.#boredApiUrl;
-    }
-
     public get dadjokeHost(): string {
         return this.#dadjokeHost;
     }
@@ -53,9 +44,5 @@ export default class Configuration {
 
     public get xRapidApiKey(): string {
         return this.#xrapidApiKey;
-    }
-
-    public get msiaCovidApiUrl(): string {
-        return this.#msiaCovidApiUrl;
     }
 }
