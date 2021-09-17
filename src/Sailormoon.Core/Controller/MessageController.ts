a415ba0eb346029bf9a6b964c9b7c414dbff5df9
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-17 19:15:32 +0800

    feat: Update message controller architecture

--- src/Sailormoon.Core/Controller/MessageController.ts
+++ src/Sailormoon.Core/Controller/MessageController.ts
@@ -1,5 +1,7 @@
 import { Message, User } from "discord.js";
 import AllChannel from "../Utility/AllChannel";
+import GreetingMessageHandler from "../Utility/GreetingMessageHandler";
+import MessageHandler from "../Utility/MessageHandler";
 import TestMessageHandler from "../Utility/TestMessageHandler";
 
 export default class MessageController {
@@ -8,15 +10,37 @@ export default class MessageController {
     #channel: AllChannel;
     #response: string;
 
+    private messageHandlers: Array<MessageHandler>;
+
     constructor(message: Message) {
         this.#message = message;
         this.#author = message.author;
         this.#channel = message.channel;
 
-        this.delegateMessage();
+        this.registerMessageHandlers();
+
+        this.sendResponse();
     }
 
-    private delegateMessage(): void {
-        new TestMessageHandler(this.#message.content, this.#channel);
+    private registerMessageHandlers(): void {
+        this.registerMessageHandler(
+            new TestMessageHandler(this.#message, this.#channel)
+        );
+        this.registerMessageHandler(
+            new GreetingMessageHandler(this.#message, this.#channel)
+        );
+    }
+
+    private registerMessageHandler(messageHandler: MessageHandler): void {
+        this.messageHandlers.push(messageHandler);
+    }
+
+    private sendResponse(): void {
+        this.messageHandlers.forEach((messageHandler: MessageHandler): void => {
+            if (messageHandler.conditionChecker()) {
+                messageHandler.execute();
+                return;
+            }
+        });
     }
 }
