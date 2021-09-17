fb9cf25052dc32ed5a33ab9102c76cc7dc36f71d
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-17 20:49:40 +0800

    refactor: Migrate link urls

--- src/Sailormoon.Core/App/Configuration.ts
+++ src/Sailormoon.Core/App/Configuration.ts
@@ -3,8 +3,6 @@ import EnvHelper from "../Utility/EnvHelper";
 export default class Configuration {
     #commandPrefix: string;
     #discordToken: string;
-    #dadjokeHost: string;
-    #jokeApiHost: string;
     #xrapidApiKey: string;
 
     private static instance: Configuration;
@@ -15,8 +13,6 @@ export default class Configuration {
         this.#commandPrefix = "?";
         this.#discordToken = EnvHelper.queryEnv("discord");
         this.#xrapidApiKey = EnvHelper.queryEnv("rapidapi");
-        this.#dadjokeHost = "dad-jokes.p.rapidapi.com";
-        this.#jokeApiHost = "jokeapi-v2.p.rapidapi.com";
     }
 
     public static getInstance(): Configuration {
@@ -32,14 +28,6 @@ export default class Configuration {
 
     public get discordToken(): string {
         return this.#discordToken;
-    }
-
-    public get dadjokeHost(): string {
-        return this.#dadjokeHost;
-    }
-
-    public get jokeApiHost(): string {
-        return this.#jokeApiHost;
     }
 
     public get xRapidApiKey(): string {
