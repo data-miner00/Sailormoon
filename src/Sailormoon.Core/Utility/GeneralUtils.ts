b2ed38a9c3694bdfa5ebe4c98b73aab5c18f0990
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-17 22:19:19 +0800

    feat: Added generator for activity

--- src/Sailormoon.Core/Utility/GeneralUtils.ts
+++ src/Sailormoon.Core/Utility/GeneralUtils.ts
@@ -1,6 +1,7 @@
 import { Message, User } from "discord.js";
 import Configuration from "../App/Configuration";
 import CommandObject from "../Command/CommandObject";
+import activities from "../Data/ActivityList";
 
 export default class GeneralUtils {
     private static configuration: Configuration = Configuration.getInstance();
@@ -21,5 +22,15 @@ export default class GeneralUtils {
 
     public static extractCommandSignature(message: Message): string {
         return message.content.split(" ")[0].slice(1) || "";
+    }
+}
+
+export function* activityGenerator() {
+    let loop_counter = 0;
+    for (;;) {
+        yield activities[loop_counter];
+
+        if (loop_counter === activities.length - 1) loop_counter = 0;
+        else loop_counter++;
     }
 }
