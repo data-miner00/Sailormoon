79cc8497efb780a1a70d0427182e9fea8ac1d2cb
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-31 16:03:33 +0800

    fix: Enable scheduler to accept date obj

--- src/Sailormoon.Core/Utility/JobScheduler.ts
+++ src/Sailormoon.Core/Utility/JobScheduler.ts
@@ -1,7 +1,7 @@
 import * as schedule from "node-schedule";
 
 export default class JobScheduler {
-    public static schdeule(cronExpression: string, callback: Function) {
-        return schedule.scheduleJob(cronExpression, callback);
+    public static schdeule(schedulable: string | Date, callback: Function) {
+        return schedule.scheduleJob(schedulable, callback);
     }
 }
