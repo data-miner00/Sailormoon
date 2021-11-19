7f19cce45c84a773aae23459e5802e64cec210be
data-miner00 <data-miner00@users.noreply.github.com>
2021-11-19 19:50:17 +0800

    Revert "Revert "Added coinmarketcap api key""
    This reverts commit 944b807b35ac96b3b7aa61a5fc568b2be3321fea.

--- src/Sailormoon.Core/Utility/EnvVariable.ts
+++ src/Sailormoon.Core/Utility/EnvVariable.ts
@@ -1,6 +1,7 @@
 enum EnvVariable {
     DISCORD_API_TOKEN = "DISCORD_API_TOKEN",
     X_RAPIDAPI_KEY = "X_RAPIDAPI_KEY",
+    "X-CMC_PRO_API_KEY" = "X-CMC_PRO_API_KEY",
 }
 
 export default EnvVariable;
