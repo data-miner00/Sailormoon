e072c52c8d3be4e447ccd51998d68831cae69755
data-miner00 <data-miner00@users.noreply.github.com>
2021-11-25 22:30:16 +0800

    feat: Enhanced crypto command

--- src/Sailormoon.Core/Command/CryptoCommand.ts
+++ src/Sailormoon.Core/Command/CryptoCommand.ts
@@ -3,6 +3,7 @@ import Command from "./Command";
 import axios from "axios";
 import Configuration from "../App/Configuration";
 import { LatestListing, LatestQuote, Quote } from "../Schemas/CoinMarketCap";
+import DateFormat from "../Utility/DateFormat";
 
 export default class CryptoCommand extends Command<MessageEmbed | string> {
     public commandSignature: string = "crypto";
@@ -39,6 +40,8 @@ export default class CryptoCommand extends Command<MessageEmbed | string> {
         const params = new Object();
         params[by] = query;
 
+        const date = new DateFormat().Now().Format("DD/MM/yyyy");
+
         try {
             const res = await axios.get(
                 "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest",
@@ -58,41 +61,60 @@ export default class CryptoCommand extends Command<MessageEmbed | string> {
             const quote = data[dynKey] as Quote;
             this.response = new MessageEmbed({
                 title: quote.name,
-                description: `Latest info for ${quote.name}`,
+                description: `Latest info for ${quote.name} in USD`,
                 url: "https://coinmarketcap.com/",
                 color: "RANDOM",
                 fields: [
                     {
                         name: "ID",
                         value: quote.id,
+                        inline: true,
                     },
                     {
                         name: "Added on",
-                        value: quote.date_added,
+                        value: new DateFormat()
+                            .FromISOString(quote.date_added)
+                            .Format("DD/MM/yyyy"),
+                        inline: true,
                     },
                     {
                         name: "Market cap",
                         value: quote.quote.USD.market_cap,
+                        inline: true,
                     },
                     {
                         name: "Price",
                         value: quote.quote.USD.price,
+                        inline: true,
                     },
                     {
                         name: "24hrs",
                         value: quote.quote.USD.percent_change_24h,
+                        inline: true,
                     },
                     {
                         name: "7days",
                         value: quote.quote.USD.percent_change_7d,
+                        inline: true,
                     },
                     {
                         name: "30days",
                         value: quote.quote.USD.percent_change_30d,
+                        inline: true,
+                    },
+                    {
+                        name: "60days",
+                        value: quote.quote.USD.percent_change_60d,
+                        inline: true,
+                    },
+                    {
+                        name: "90days",
+                        value: quote.quote.USD.percent_change_90d,
+                        inline: true,
                     },
                 ],
                 footer: {
-                    text: "Powered by Coinmarketcap 💰",
+                    text: `Powered by Coinmarketcap • ${date}`,
                 },
             });
         } catch (error: unknown) {
