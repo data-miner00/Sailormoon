537397290e5c5bea958ea0242d9e9ab48854a683
data-miner00 <data-miner00@users.noreply.github.com>
2021-12-05 18:03:19 +0800

    feat: Added Hdl message handler

---
+++ src/Sailormoon.Core/MessageHandler/HdlMessageHandler.ts
@@ -0,0 +1,12 @@
+import MessageHandler from "./MessageHandler";
+import HdlReplyData from "../Data/HdlReplyData";
+import Randomizer from "../Utility/Randomizer";
+
+export default class HdlMessageHandler extends MessageHandler {
+    public conditionChecker(): boolean {
+        return this.message.content.toLowerCase().includes("hdl");
+    }
+    public execute(): void {
+        this.message.reply(Randomizer.RandomElement(HdlReplyData));
+    }
+}
