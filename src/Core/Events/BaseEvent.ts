1f3150ea66f4791371f9755f99f5880a75a96830
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-18 01:02:33 +0800

    feat: Added event contracts

---
+++ src/Core/Events/BaseEvent.ts
@@ -0,0 +1,16 @@
+import { Client, ClientEvents } from "discord.js";
+import Application from "../../App/Application";
+import IEvent from "./IEvent";
+
+export default abstract class BaseEvent implements IEvent {
+    public eventName: keyof ClientEvents;
+    public abstract EventHandler(
+        ...args: ClientEvents[keyof ClientEvents]
+    ): void;
+    public bot: Client;
+
+    public constructor(eventName: keyof ClientEvents) {
+        this.eventName = eventName;
+        this.bot = Application.GetInstance()._bot;
+    }
+}
