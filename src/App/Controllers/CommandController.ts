30de6c6ff017d846f3ec43bca11b7698bd3efdb7
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-19 23:11:04 +0800

    feat: Added initial help command

--- src/App/Controllers/CommandController.ts
+++ src/App/Controllers/CommandController.ts
@@ -1,9 +1,11 @@
 import Controller from "../../Core/Controller/Controller";
 import CommandHandler from "../../Core/Handlers/CommandHandler";
 import { PingCommand } from "../Handlers/Command/";
+import HelpCommand from "../Handlers/Command/HelpCommand";
 
 export default class CommandController extends Controller<CommandHandler> {
     protected RegisterHandlers(): void {
         this.RegisterHandler(new PingCommand(this.message));
+        this.RegisterHandler(new HelpCommand(this.message));
     }
 }
