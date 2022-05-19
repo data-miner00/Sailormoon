3f4bcea51a3673948bb1032d30b5b6ab85959676
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-19 22:44:42 +0800

    feat: Ping to return ms

--- src/App/Handlers/Command/PingCommand.ts
+++ src/App/Handlers/Command/PingCommand.ts
@@ -1,10 +1,16 @@
 import CommandHandler from "../../../Core/Handlers/CommandHandler";
+import Application from "../../Application";
 
 export default class PingCommand extends CommandHandler {
     public ConditionChecker(): boolean {
         return this.message.content === "kyle?ping";
     }
     public Handle(): void {
-        this.message.channel.send("pong");
+        this.message.channel.send("pong!").then((message) => {
+            const latency: number = Math.round(
+                Application.GetInstance()._bot.ws.ping
+            );
+            message.edit("pong! `" + latency + "ms`");
+        });
     }
 }
