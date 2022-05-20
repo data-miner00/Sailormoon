6c6c299324a3300b34d7a9bd2ed1d5f7f2295467
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-20 21:06:23 +0800

    docs: Added comments or parser

--- src/App/Helpers/CommandParser.ts
+++ src/App/Helpers/CommandParser.ts
@@ -4,19 +4,31 @@ export function Parse(input: string) {
     const quotedStringRegex = /["'](.*?)["']/gm;
 
     /*
+     * From the input command, split by whitespace and get only up to first 3 items
+     * as we will care about the other arguments later.
      *
+     * Example:
+     * Given "prefix command hello --options value --option2",
+     * we get [prefix, command, hello] and the prefix is ignored by _.
      */
     const [_, command, subject] = input.split(" ", 3);
 
     /*
+     * For the subject of the command with spaces in between, the command
+     * will need to enclose the subject with either single quotes or double quotes.
      *
+     * If the search does not found any quoted strings, it will return null.
      */
     const quotedStringMatch = quotedStringRegex.exec(input);
-    console.log(quotedStringMatch);
+
     /*
+     * Storage for the flags in key value pairs.
      *
+     * Example:
+     * Given "--option1 val1 --option2 val2", it will resolve into
+     * [{name: "option1", value: "val1"}, {name: "option2", value: "val2"}]
      */
-    const flags = [];
+    const flags: Array<{ name: string; value: string }> = [];
 
     let matches: RegExpExecArray;
 
