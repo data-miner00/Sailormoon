2003b6d18f15735908a00d9ac99557118dddd5e2
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-20 21:29:45 +0800

    feat: Added hash command

--- src/App/Controllers/CommandController.ts
+++ src/App/Controllers/CommandController.ts
@@ -1,11 +1,13 @@
 import Controller from "../../Core/Controller/Controller";
 import CommandHandler from "../../Core/Handlers/CommandHandler";
 import { PingCommand } from "../Handlers/Command/";
+import HashCommand from "../Handlers/Command/HashCommand";
 import HelpCommand from "../Handlers/Command/HelpCommand";
 
 export default class CommandController extends Controller<CommandHandler> {
     protected RegisterHandlers(): void {
         this.RegisterHandler(new PingCommand(this.message));
         this.RegisterHandler(new HelpCommand(this.message));
+        this.RegisterHandler(new HashCommand(this.message));
     }
 }
