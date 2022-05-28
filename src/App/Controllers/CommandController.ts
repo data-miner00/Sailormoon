99d1653a99ae464fc062a2861d5c2040373413f7
data-miner00 <data-miner00@users.noreply.github.com>
2022-05-28 23:28:20 +0800

    feat: Added relay command

--- src/App/Controllers/CommandController.ts
+++ src/App/Controllers/CommandController.ts
@@ -1,11 +1,17 @@
 import Controller from "../../Core/Controller/Controller";
 import CommandHandler from "../../Core/Handlers/CommandHandler";
-import { PingCommand, HashCommand, HelpCommand } from "../Handlers/Command/";
+import {
+    PingCommand,
+    HashCommand,
+    HelpCommand,
+    RelayMessageCommand,
+} from "../Handlers/Command/";
 
 export default class CommandController extends Controller<CommandHandler> {
     protected RegisterHandlers(): void {
         this.RegisterHandler(new PingCommand(this.message));
         this.RegisterHandler(new HelpCommand(this.message));
         this.RegisterHandler(new HashCommand(this.message));
+        this.RegisterHandler(new RelayMessageCommand(this.message));
     }
 }
--- src/App/Handlers/Command/index.ts
+++ src/App/Handlers/Command/index.ts
@@ -1,3 +1,4 @@
 export { default as PingCommand } from "./PingCommand";
 export { default as HelpCommand } from "./HelpCommand";
 export { default as HashCommand } from "./HashCommand";
+export { default as RelayMessageCommand } from "./RelayMessageCommand";
